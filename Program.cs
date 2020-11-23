7edd973f994adadb736c4dbfc3b55e8039c97426
Iryna <ortofosfatnyi@gmail.com>
2020-11-23 14:20:17 +0200

    "We must find roots and fixt 'public static double getD' "

--- Program.cs
+++ Program.cs
@@ -25,5 +25,11 @@ namespace HW7
             double D = Math.Pow(b, 2) - 4 * a * c;
             return D;
         }
+
+        public static void getRoots(double a, double b, double c, out double x1, out double x2)
+        {
+            x1 = (-b + Math.Sqrt(getD(a, b, c))) / (2 * a);
+            x2 = (-b - Math.Sqrt(getD(a, b, c))) / (2 * a);
+        }
     }
 }
